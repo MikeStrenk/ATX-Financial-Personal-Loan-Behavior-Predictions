fdf051d5cbb3ef324cf47696fa7faa94403d1c2b
MikeStrenk <mikestrenk@gmail.com>
2018-10-05 18:03:55 -0500

    Ready for heroku

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
 from flask import Flask, render_template, flash, redirect, request, url_for
+from sklearn.externals import joblib
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 from forms import ApplicationForm
+
+
+tfidf_model = None
+
+
+def load_tfidf_model():
+    global tfidf_model
+    tfidf_model = joblib.load('data/model3.pkl')
+
+
+def convert_to_binary(x):
+    if x == 'No':
+        return 0
+    if x == 'Yes':
+        return 1
+    else:
+        raise ValueError
+
+
+def convert_credit_grade(grade):
+    grade_dict = {
+        '>750': 1,
+        '>700': 2,
+        '>650': 3,
+        '>625': 4,
+        '>600': 5,
+        '>550': 6,
+        "Bad Credit": 7
+    }
+    return grade_dict[grade]
 
 
 @app.route('/')
@@ -27,49 +58,45 @@ def return_loan_application():
     form = ApplicationForm()
     message = ''
     if form.validate_on_submit():
-        first_name = form.firstname.data
-        last_name = form.lastname.data
+        age = form.age.data
+        occupation = form.occupation.data
 
-        grade = form.grade.data
+        grade = convert_credit_grade(form.grade.data)
         Years_At_present_Employment = form.Years_At_present_Employment.data
 
         expenses = form.expenses.data
         income = form.income.data
-        dti = (expenses / (income / 12)*100)
+        dti = round((expenses / (income / 12)*100), 2)
 
-        Delinquency = form.Delinquency.data
-        Collections = form.Collections.data
-        Derogatory = form.Derogatory.data
-        Housing_MORTGAGE = form.Housing_MORTGAGE.data
-        Housing_OWN = form.Housing_OWN.data
-        Housing_RENT = form.Housing_RENT.data
+        Delinquency = convert_to_binary(form.Delinquency.data)
+        Collections = convert_to_binary(form.Collections.data)
+        Derogatory = convert_to_binary(form.Derogatory.data)
+        Housing = form.Housing.data
 
-        print(first_name)
-        print(last_name)
+        application_data = [[grade, dti, Delinquency, Collections, Derogatory]]
 
-        print(grade)
-        print(Years_At_present_Employment)
-        print(expenses)
-        print(income)
-        print(dti)
-        print(Delinquency)
-        print(Collections)
-        print(Derogatory)
-        print(Housing_MORTGAGE)
-        print(Housing_OWN)
-        print(Housing_RENT)
+        probabs_result = tfidf_model.predict_proba(application_data)
+
+        default_chance = probabs_result[1][0][1]
+
+        print('Chance of default: {}'.format(default_chance*100))
 
         # db logic goes here
 
-        print("\nData received. Now redirecting ...")
-        return redirect(url_for('return_loan_application'))
+        if default_chance > .3:
+            message = 'However you have been rejected :('
+        else:
+            message = 'You have been accepted! :)'
 
-    return render_template('loan_application.html', form=form, message=message)
+        return render_template('application_complete.html', message=message)
+
+    return render_template('loan_application.html', form=form)
 
 
-# @app.route('/application_complete', methods=['GET', 'POST'])
-# def return_application_complete():
-#     return render_template('application_complete.html', form=form)
+@app.route('/application_complete', methods=['GET', 'POST'])
+def return_application_complete(message):
+    message = message
+    return render_template('application_complete.html', message=message)
 
 
 # @app.route('/handle_data', methods=['GET', 'POST'])
@@ -85,4 +112,13 @@ def return_loan_application():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    try:
+        load_tfidf_model()
+        print("Model loaded")
+
+    except Exception as e:
+        print("Model loading failed")
+        print(str(e))
+
+    app.run(debug=False)
--- forms.py
+++ forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import DataRequired, AnyOf, NoneOf
 
 
 class ApplicationForm(FlaskForm):
-    firstname = StringField("First Name: ", validators=[DataRequired()])
-    lastname = StringField("Last Name: ", validators=[DataRequired()])
+    age = IntegerField("Age: ", validators=[DataRequired()])
+    occupation = StringField("Occupation: ", validators=[DataRequired()])
     grade = SelectField("Credit Score: ", choices=[
         ('Choose', 'Choose'),
         ('>750', '>750'),
@@ -15,7 +15,7 @@ class ApplicationForm(FlaskForm):
         ('>600', '>600'),
         ('>550', '>550'),
         ("Bad Credit", "Bad Credit")],
-        validators=[AnyOf(['>750', '>700', '>650', '>650', '>600', '>550', "Bad Credit"])])
+        validators=[AnyOf(['>750', '>700', '>650', '>625', '>600', '>550', "Bad Credit"])])
     Years_At_present_Employment = IntegerField(
         "Years at present employment: ", validators=[DataRequired()])
     expenses = IntegerField(
@@ -28,9 +28,16 @@ class ApplicationForm(FlaskForm):
         "Have you ever been in collections on a loan?: ", choices=[('Choose', 'Choose'), ('Yes', 'Yes'), ('No', 'No')], validators=[AnyOf(['Yes', 'No'])])
     Derogatory = SelectField(
         "Do you have a derogatory item on a loan?: ", choices=[('Choose', 'Choose'), ('Yes', 'Yes'), ('No', 'No')], validators=[AnyOf(['Yes', 'No'])])
-    Housing_MORTGAGE = SelectField(
-        "Mortgage?: ", choices=[('Choose', 'Choose'), ('Yes', 'Yes'), ('No', 'No')], validators=[AnyOf(['Yes', 'No'])])
-    Housing_OWN = SelectField("Own your Home?: ", choices=[(
-        'Choose', 'Choose'), ('Yes', 'Yes'), ('No', 'No')], validators=[AnyOf(['Yes', 'No'])])
-    Housing_RENT = SelectField("Rent your Home?: ", choices=[(
-        'Choose', 'Choose'), ('Yes', 'Yes'), ('No', 'No')], validators=[AnyOf(['Yes', 'No'])])
+    Housing = SelectField(
+        "What is your housing situation?: ",
+        choices=[('Choose', 'Choose'),
+                 ('Own my home', 'Own my home'),
+                 ('Mortgage', 'Mortgage'),
+                 ('Rent', 'Rent'),
+                 ('Live with Parents',
+                  'Live with Parents')],
+        validators=[AnyOf([
+            'Own my Home',
+            'Mortgage',
+            'Rent',
+            'Live with Parents'])])
