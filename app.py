b9026d63e9fb706283a827de390bb6f35b165f0e
MikeStrenk <mikestrenk@gmail.com>
2018-10-04 20:41:43 -0500

    Application form working and returning the values back to flask

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, flash, redirect, request
+from flask import Flask, render_template, flash, redirect, request, url_for
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from forms import TestForm
+from forms import ApplicationForm
 
 
 @app.route('/')
@@ -24,13 +24,52 @@ def return_loan_history():
 
 @app.route('/loan_application', methods=['GET', 'POST'])
 def return_loan_application():
-    form = TestForm()
-    return render_template('loan_application.html', form=form)
+    form = ApplicationForm()
+    message = ''
+    if form.validate_on_submit():
+        first_name = form.firstname.data
+        last_name = form.lastname.data
+
+        grade = form.grade.data
+        Years_At_present_Employment = form.Years_At_present_Employment.data
+
+        expenses = form.expenses.data
+        income = form.income.data
+        dti = (expenses / (income / 12)*100)
+
+        Delinquency = form.Delinquency.data
+        Collections = form.Collections.data
+        Derogatory = form.Derogatory.data
+        Housing_MORTGAGE = form.Housing_MORTGAGE.data
+        Housing_OWN = form.Housing_OWN.data
+        Housing_RENT = form.Housing_RENT.data
+
+        print(first_name)
+        print(last_name)
+
+        print(grade)
+        print(Years_At_present_Employment)
+        print(expenses)
+        print(income)
+        print(dti)
+        print(Delinquency)
+        print(Collections)
+        print(Derogatory)
+        print(Housing_MORTGAGE)
+        print(Housing_OWN)
+        print(Housing_RENT)
+
+        # db logic goes here
+
+        print("\nData received. Now redirecting ...")
+        return redirect(url_for('return_loan_application'))
+
+    return render_template('loan_application.html', form=form, message=message)
 
 
-@app.route('/application_complete', methods=['GET', 'POST'])
-def return_loan_application():
-    return render_template('application_complete.html', form=form)
+# @app.route('/application_complete', methods=['GET', 'POST'])
+# def return_application_complete():
+#     return render_template('application_complete.html', form=form)
 
 
 # @app.route('/handle_data', methods=['GET', 'POST'])
--- forms.py
+++ forms.py
@@ -1,26 +1,36 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, Form, BooleanField, StringField, PasswordField, validators, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import TextField, SelectField, IntegerField, Form, BooleanField, StringField, PasswordField, validators, SubmitField
+from wtforms.validators import DataRequired, AnyOf, NoneOf
 
 
-class TestForm(FlaskForm):
-    first_name = TextField("firstName")
-    last_name = TextField("lastName")
-
-
-class LoginForm(FlaskForm):
-    firstname = StringField('firstname', validators=[DataRequired()])
-    lastname = PasswordField('lastname', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Submit Application')
-
-
-class RegistrationForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
-    password = PasswordField('New Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')
-    ])
-    confirm = PasswordField('Repeat Password')
-    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+class ApplicationForm(FlaskForm):
+    firstname = StringField("First Name: ", validators=[DataRequired()])
+    lastname = StringField("Last Name: ", validators=[DataRequired()])
+    grade = SelectField("Credit Score: ", choices=[
+        ('Choose', 'Choose'),
+        ('>750', '>750'),
+        ('>700', '>700'),
+        ('>650', '>650'),
+        ('>625', '>625'),
+        ('>600', '>600'),
+        ('>550', '>550'),
+        ("Bad Credit", "Bad Credit")],
+        validators=[AnyOf(['>750', '>700', '>650', '>650', '>600', '>550', "Bad Credit"])])
+    Years_At_present_Employment = IntegerField(
+        "Years at present employment: ", validators=[DataRequired()])
+    expenses = IntegerField(
+        "Monthly Expenses: ", validators=[DataRequired()])
+    income = IntegerField(
+        "Annual Salary: ", validators=[DataRequired()])
+    Delinquency = SelectField(
+        "Have you ever been delinquent on a loan?: ", choices=[('Choose', 'Choose'), ('Yes', 'Yes'), ('No', 'No')], validators=[AnyOf(['Yes', 'No'])])
+    Collections = SelectField(
+        "Have you ever been in collections on a loan?: ", choices=[('Choose', 'Choose'), ('Yes', 'Yes'), ('No', 'No')], validators=[AnyOf(['Yes', 'No'])])
+    Derogatory = SelectField(
+        "Do you have a derogatory item on a loan?: ", choices=[('Choose', 'Choose'), ('Yes', 'Yes'), ('No', 'No')], validators=[AnyOf(['Yes', 'No'])])
+    Housing_MORTGAGE = SelectField(
+        "Mortgage?: ", choices=[('Choose', 'Choose'), ('Yes', 'Yes'), ('No', 'No')], validators=[AnyOf(['Yes', 'No'])])
+    Housing_OWN = SelectField("Own your Home?: ", choices=[(
+        'Choose', 'Choose'), ('Yes', 'Yes'), ('No', 'No')], validators=[AnyOf(['Yes', 'No'])])
+    Housing_RENT = SelectField("Rent your Home?: ", choices=[(
+        'Choose', 'Choose'), ('Yes', 'Yes'), ('No', 'No')], validators=[AnyOf(['Yes', 'No'])])
